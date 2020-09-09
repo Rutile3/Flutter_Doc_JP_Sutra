4d3c7f1ea3487450ee44712ed00f0987b071fd73
Rutile3 <Rutile3@outlook.jp>
2020-09-09 14:07:16 +0900

    ✨ BottomNavigationBarを作成

---
+++ basic/navigation/bottom_navigation_bar/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _currentIndex = 0;
+  final _pageWidgets = [
+    PageWidget(color:Colors.white, title:'Home'),
+    PageWidget(color:Colors.blue, title:'Album'),
+    PageWidget(color:Colors.orange, title:'Chat'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BottomNavigationBar'),
+      ),
+      body: _pageWidgets.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_album), title: Text('Album')),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('Chat')),
+        ],
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) => setState(() => _currentIndex = index );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => new _MainPageState();
+}
+
+class PageWidget extends StatelessWidget {
+  final Color color;
+  final String title;
+
+  PageWidget({Key key, this.color, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
+}
