e3106dfe3a85ca29a5cfdb489aac444f734dba82
Rutile3 <Rutile3@outlook.jp>
2020-09-09 18:26:53 +0900

    ✨ 前に戻ったりができない画面遷移を作成

---
+++ basic/routing/routing02/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+      routes: <String, WidgetBuilder> {
+        '/home': (BuildContext context) => new MainPage(),
+        '/subpage': (BuildContext context) => new SubPage()
+      },
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              Text('Main'),
+              RaisedButton(onPressed: () => Navigator.of(context).pushReplacementNamed("/subpage"), child: new Text('Subページへ'),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SubPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              Text('Sub'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
